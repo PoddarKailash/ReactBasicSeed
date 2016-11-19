96e040e756aebe3f491f5acf360922f6b31715d0
Kailash Poddar <kailashpoddar@Kailashs-MacBook-Pro.local>
2016-11-19 07:52:09 +0530

    Basic Hello World

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+// create component
 export default class App extends Component {
   render() {
     return (
       <div>
-      <h1> React Hello World </h1></div>
+      	<h1> React Hello World </h1>
+      </div>
     );
   }
 }
